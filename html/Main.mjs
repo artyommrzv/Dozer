93f4d61df352a4dcfa66a950be9e1e3db3958c90
Melonjazz13 <130756684+Melonjazz13@users.noreply.github.com>
2023-11-21 21:06:14 +0400

    added Data and update Main

--- html/Main.mjs
+++ html/Main.mjs
@@ -12,7 +12,9 @@ class Main {
         this.assets.load( () => {
             this.initScene();
             this.initCamera();
+            this.initLight();
             this.initRenderer();
+            this.gameLoop();            
         });        
     }
 
@@ -26,6 +28,25 @@ class Main {
         this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
         this.scene.add(this.camera);
     }
+
+    initLight() {
+        let light = new THREE.DirectionalLight( 0xffffff, 0.8 );
+        light.castShadow = true;
+        light.position.set( -1, 5, 2 );
+        this.scene.add( light );
+    
+        light.shadow.camera.left = -20;
+        light.shadow.camera.right = 20;
+        light.shadow.camera.top = 20;
+        light.shadow.camera.bottom = -20;
+        light.shadow.radius = 10;
+        
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
+    
+        let ambientLight = new THREE.AmbientLight( 0xffffff, 0.4 ); 
+        this.scene.add( ambientLight );
+    };
     
     initRenderer() {
         this.renderer = new THREE.WebGLRenderer({
@@ -39,6 +60,11 @@ class Main {
         document.body.appendChild(this.renderer.domElement);
 
         this.renderer.render(this.scene, this.camera);
+    }
+
+    gameLoop = () => {
+        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(this.gameLoop); 
     }
 
 }
